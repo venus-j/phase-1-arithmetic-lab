991fa5f24017435f83d013f36a7299798bdfce6c
venus-j <chumbavenus24@gmail.com>
2024-07-10 11:41:17 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+const num1 = 2;
+const num2 =  31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 20;
+const num4 = 8;
+const mod =num3 % num4;
+
+const max = Math.max(12, 20, 14, 16, 16);
